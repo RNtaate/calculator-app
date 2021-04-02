e5e942d43bd14a958e254e79d7af167f4af00a75
RNtaate <royhyde14@gmail.com>
2021-04-02 12:33:11 +0300

    Create button, buttonPanel, display components, import them in app.js

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let ButtonPanel = () => {
+  return(
+    <div>
+      <h1>My Button Panel</h1>
+    </div>
+  )
+}
+
+export default ButtonPanel;
---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let Display = () => {
+  return(
+    <input type="text"/>
+  );
+}
+
+export default Display;
