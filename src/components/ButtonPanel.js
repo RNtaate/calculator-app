603f270a5029d1e936c4dd05f62b10f90181d154
RNtaate <royhyde14@gmail.com>
2021-04-02 12:58:26 +0300

    Import button component, create displayButton function, add calculator buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import Button from './Button';
 
-let ButtonPanel = () => {
+let ButtonPanel = () => { 
+
+  let displayButton = (myName) => {
+    return(
+      <Button name={myName}/>
+    )
+  }
+
   return(
-    <div>
-      <h1>My Button Panel</h1>
+    <div className='calculator-panel'>
+
+      <div className='group1'>
+        {displayButton('AC')}
+        {displayButton('+/-')}
+        {displayButton('%')}
+        {displayButton('÷')}
+      </div>
+      <div className='group2'>
+        {displayButton('7')}
+        {displayButton('8')}
+        {displayButton('9')}
+        {displayButton('X')}
+      </div>   
+      <div className='group3'>
+        {displayButton('4')}
+        {displayButton('5')}
+        {displayButton('6')}
+        {displayButton('-')}
+      </div>   
+      <div className='group4'>
+        {displayButton('1')}
+        {displayButton('2')}
+        {displayButton('3')}
+        {displayButton('+')}
+      </div>  
+      <div className='group5'>
+        {displayButton('0')}
+        {displayButton('.')}
+        {displayButton('=')}
+      </div>      
+
     </div>
   )
 }
