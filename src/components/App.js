8e9c6cce79b5d425fd603ef91191d358e3830de3
RNtaate <royhyde14@gmail.com>
2021-04-04 18:31:46 +0300

    Update App function to class component, create handleClick function to update state using calculate function

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,51 @@
+import React, {Component} from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+
+    this.handleClick = this.handleClick.bind(this); 
+  }
+
+  handleClick(buttonName) {
+    let calcData = Object.assign({}, this.state);
+    calcData = calculate(buttonName, calcData);
+    this.setState({
+      total: calcData.total,
+      next: calcData.next,
+      operation: calcData.operation
+    })
+  }
+
+  render() {
+    let displayResult = '';
+    if((this.state.total === null) && (this.state.next === null)){
+        displayResult = '0'
+    }else{
+      let total = this.state.total === null ? '' : this.state.total;
+      let myNext = this.state.next === null ? '' : this.state.next;
+      let operation = this.state.operation === null ? '' : this.state.operation;
+
+      displayResult = `${total} ${operation} ${myNext}`;
+    }
+
+    return (
+      <>
+        <Display result={displayResult}/>
+        <ButtonPanel clickHandler={this.handleClick}/>
+      </>
+    );
+  }
 }
 
 export default App;
