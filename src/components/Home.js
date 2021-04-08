cb556e09bace50cde315317ebff7dc47b58a3353
RNtaate <royhyde14@gmail.com>
2021-04-08 17:07:32 +0300

    Create home component page.

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let Home = () => {
+  return(
+    <div>
+      <h2>Welcome to our page!</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla consectetur nostrum veritatis quos? At ipsum reiciendis unde reprehenderit? Voluptatibus quam dolorum recusandae hic assumenda minus officiis facere explicabo cumque saepe!</p>
+      <br></br>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla consectetur nostrum veritatis quos? At ipsum reiciendis unde reprehenderit? Voluptatibus quam dolorum recusandae hic assumenda minus officiis facere explicabo cumque saepe!</p>
+    </div>
+  );
+}
+
+export default Home;
