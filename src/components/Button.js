bcb0c5a21ee0dc3895ac85815c68ab614bff70a3
RNtaate <royhyde14@gmail.com>
2021-04-08 15:35:14 +0300

    Implement passing of styling through props to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const { name } = props;
+  const { name, color, wide } = props;
 
   const handleClick = (e) => {
     props.clickHandler(e.target.value);
   };
 
   return (
-    <button type="button" onClick={handleClick} value={name}>{name}</button>
+    <button type="button" onClick={handleClick} value={name}  style={{ backgroundColor: color ? color : '', width: wide ? "50%" : "25%" }}>{name}</button>
   );
 };
 
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,40 +8,40 @@ const ButtonPanel = (props) => {
     myHandler(buttonName);
   };
 
-  const displayButton = (myName) => (
-    <Button name={myName} clickHandler={handleClick} />
+  const displayButton = (myName, myColor = null, myWide = false) => (
+    <Button name={myName} clickHandler={handleClick} color={myColor} wide={myWide}/>
   );
 
   return (
     <div className="calculator-panel">
 
       <div className="group1">
-        {displayButton('AC')}
-        {displayButton('+/-')}
-        {displayButton('%')}
+        {displayButton('AC', '#e0e0e0')}
+        {displayButton('+/-', '#e0e0e0')}
+        {displayButton('%', '#e0e0e0')}
         {displayButton('÷')}
       </div>
       <div className="group2">
-        {displayButton('7')}
-        {displayButton('8')}
-        {displayButton('9')}
+        {displayButton('7', '#e0e0e0')}
+        {displayButton('8', '#e0e0e0')}
+        {displayButton('9', '#e0e0e0')}
         {displayButton('X')}
       </div>
       <div className="group3">
-        {displayButton('4')}
-        {displayButton('5')}
-        {displayButton('6')}
+        {displayButton('4', '#e0e0e0')}
+        {displayButton('5', '#e0e0e0')}
+        {displayButton('6', '#e0e0e0')}
         {displayButton('-')}
       </div>
       <div className="group4">
-        {displayButton('1')}
-        {displayButton('2')}
-        {displayButton('3')}
+        {displayButton('1', '#e0e0e0')}
+        {displayButton('2', '#e0e0e0')}
+        {displayButton('3', '#e0e0e0')}
         {displayButton('+')}
       </div>
       <div className="group5">
-        {displayButton('0')}
-        {displayButton('.')}
+        {displayButton('0', '#e0e0e0', true)}
+        {displayButton('.', '#e0e0e0')}
         {displayButton('=')}
       </div>
 
