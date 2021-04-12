f1b88531c666307efa545142f0daa1568e75c99c
RNtaate <royhyde14@gmail.com>
2021-04-12 13:57:47 +0300

    Implement props in quote function component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const Quote = () => (
-  <p
-    className="quote-par"
-    style={{
-      fontSize: '25px',
-      padding: '20px 100px',
-    }}
-  >
-    Mathematics is not about numbers, equations, computations,
-    or algorithms: it is about understanding. –William Paul Thurston
-  </p>
-);
+const Quote = (props) => {
+  return (
+    <p
+      className="quote-par"
+      style={{
+        fontSize: '25px',
+        padding: '20px 100px',
+      }} data-testid="quote"
+    >
+      {props.myQuote}
+    </p>
+  )
+};
+
+Quote.defaultProps = {
+  myQuote: "Mathematics is not about numbers, equations, computations,or algorithms: it is about understanding. –William Paul Thurston"
+}
 
 export default Quote;
