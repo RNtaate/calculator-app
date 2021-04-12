3f7b64dbc3f8e0405d547dbe3250cccc9c40e147
RNtaate <royhyde14@gmail.com>
2021-04-12 17:05:48 +0300

    Update unit test for calculate.js file.

--- src/logic/__test__/calculate.test.js
+++ src/logic/__test__/calculate.test.js
@@ -22,3 +22,24 @@ test('it should return a value of type object', () => {
   const newData = calculate('', myData);
   expect(typeof newData).toBe('object');
 });
+
+test('returns an object with calculated property values basing on buttonName provided as argument', () => {
+  let newData = calculate('3', myData);
+  expect(newData.next).toBe('3');
+  expect(newData.next).not.toBeNull();
+
+  myData.next = '3';
+  newData = calculate('+', myData);
+  expect(newData.total).toBe('3');
+  expect(newData.total).not.toBeNull();
+  expect(newData.next).toBeNull();
+  expect(newData.operation).toBe('+');
+
+  myData.total = '3';
+  myData.operation = '+';
+  myData.next = '2';
+  newData = calculate('=', myData);
+  expect(newData.total).toBe('5');
+  expect(newData.next).toBeNull();
+  expect(newData.operation).toBeNull();
+});
